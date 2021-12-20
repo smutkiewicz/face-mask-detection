c994de28612ff6824d0c675ce06da20fc7b98411
Tomasz Miazga <tom3k.miazga@gmail.com>
2021-12-20 16:26:36 +0100

    Add few test images and test function

--- face_detector/face_detector.py
+++ face_detector/face_detector.py
@@ -7,9 +7,24 @@ IMAGES_FOLDER_PATH = r'images/'
 RESIZED_IMAGES_FOLDER_PATH = r'resized_images/'
 IMAGE_SIZE = 224
 
+# test function used to calibrate classifier
+def show_image_with_indications():
+	image = cv2.imread(IMAGES_FOLDER_PATH + 'przystojniak_1.jpg')
+	classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+	
+	boxes = classifier.detectMultiScale(image, 1.2, 5)
+	for box in boxes:
+		x, y, width, height = box
+		x2, y2 = x + width, y + height
+		cv2.rectangle(image, (x, y), (x2, y2), (0,0,255), 1)
+
+	cv2.imshow('face detection', image)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
 
 def resize_image(image, x, x2, y, y2):
-	spread = 0
+	spread = 10
 	cropped_image = image[y - spread : y2 + spread, x - spread : x2 + spread]
 	dim = (IMAGE_SIZE, IMAGE_SIZE)
 
@@ -25,7 +40,7 @@ def save_resized_face_image(image, image_index, box_index, x, x2, y, y2):
 
 
 def handle_image(image: any, image_index, classifier: any):
-	boxes = classifier.detectMultiScale(image)
+	boxes = classifier.detectMultiScale(image, 1.2, 5)
 	box_index = 1
 
 	for box in boxes:
@@ -41,18 +56,29 @@ def clear_resized_images():
          os.remove(f)
 
 
+def main() :
+	clear_resized_images()
+
+	images_paths = list(filter(lambda k: 'jpg' in k, os.listdir(IMAGES_FOLDER_PATH)))
+	images_paths = [IMAGES_FOLDER_PATH + path for path in images_paths]
+
+	cascade_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+	image_index = 1
+
+	for path in images_paths:
+		handle_image(cv2.imread(path), image_index, cascade_classifier)
+		image_index += 1
+
+	# uncomment to test a single image
+	#show_image_with_indications()
+
+
+
+
 # ==========================================================================================================
 
 
-clear_resized_images()
 
-images_paths = list(filter(lambda k: 'jpg' in k, os.listdir(IMAGES_FOLDER_PATH)))
-images_paths = [IMAGES_FOLDER_PATH + path for path in images_paths]
 
-cascade_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-image_index = 1
-
-for path in images_paths:
-	handle_image(cv2.imread(path), image_index, cascade_classifier)
-	image_index += 1
+main()
