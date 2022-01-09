ad2d6813a5bcf5bc9484e2e581999c3cd9785dc4
Tomasz Miazga <tom3k.miazga@gmail.com>
2022-01-09 16:03:06 +0100

    Add testing code

--- data-augmentation/data-augmentation.py
+++ data-augmentation/data-augmentation.py
@@ -2,14 +2,21 @@ import cv2
 import os
 import glob
 
-def horizontal_flip(image):
+
+TEST_IMAGES_FOLDER_PATH = r'test-images/'
+MODIFIED_IMAGES_FOLDER_PATH = r'modified-images/'
+
+
+def horizontal_flip(image: any):
     return cv2.flip(image, 1)
 
-def vertical_flip(image):
+
+def vertical_flip(image: any):
     return cv2.flip(image, 0)
 
-def increase_brightness(image):
-    mutation = 20
+
+def increase_brightness(image: any):
+    mutation = 70
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     h, s, v = cv2.split(hsv)
 
@@ -23,3 +30,51 @@ def increase_brightness(image):
     return final_image
 
 
+def clear_modified_images():
+	files = glob.glob(MODIFIED_IMAGES_FOLDER_PATH + '*.jpg')
+	for f in files:
+         os.remove(f)
+
+
+def save_image(image: any, filename):
+    os.chdir(MODIFIED_IMAGES_FOLDER_PATH)
+    cv2.imwrite(filename + ".jpg", image)
+    os.chdir('..')
+
+
+def modify_and_save_images(image: any, image_index: int, brighten = False):
+    image[brighten] = increase_brightness(image)
+    suffix = "-b" if brighten else "-"
+
+    save_image(image, str(image_index) + suffix)
+    save_image(horizontal_flip(image), str(image_index) + suffix + "h")
+    save_image(vertical_flip(image), str(image_index) + suffix + "v")
+    save_image(horizontal_flip(vertical_flip(image)), str(image_index) + suffix + "hv")
+
+
+def handle_image(image: any, image_index: int):
+    modify_and_save_images(image, image_index)
+    modify_and_save_images(image, image_index, True)
+
+
+def main():
+    clear_modified_images()
+
+    images_paths = list(filter(lambda k: 'jpg' in k, os.listdir(TEST_IMAGES_FOLDER_PATH)))
+    images_paths = [TEST_IMAGES_FOLDER_PATH + path for path in images_paths]
+
+    image_index = 1
+
+    for path in images_paths:
+        handle_image(cv2.imread(path), image_index)
+        image_index += 1
+
+
+
+
+# ==========================================================================================================
+
+
+
+
+main()
